998f228fbf8c71d4933314865228fdcec2ffdd82
k_sergej <50540573+SergejKolesnik@users.noreply.github.com>
2026-04-12 11:58:17 +0300

    Create weather_service.py

---
+++ weather_service.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+@st.cache_data(ttl=3600)
+def fetch_weather_data():
+    """
+    Отримує свіжі метеодані через API Visual Crossing.
+    """
+    try:
+        api_key = st.secrets["WEATHER_API_KEY"]
+        url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/47.631494,34.348690/next10days?unitGroup=metric&elements=datetime,temp,cloudcover,solarradiation,windspeed,precipprob&key={api_key}&contentType=json"
+        
+        res = requests.get(url, timeout=10)
+        if res.status_code == 200:
+            data = res.json()
+            h_list = []
+            for d in data['days']:
+                for hr in d['hours']:
+                    h_list.append({
+                        'Time': pd.to_datetime(f"{d['datetime']} {hr['datetime']}"),
+                        'Hour': int(hr['datetime'].split(':')[0]),
+                        'Rad': hr.get('solarradiation', 0),
+                        'CloudCover': hr.get('cloudcover', 0),
+                        'Temp': hr.get('temp', 0),
+                        'WindSpeed': hr.get('windspeed', 0),
+                        'PrecipProb': hr.get('precipprob', 0)
+                    })
+            df = pd.DataFrame(h_list)
+            # Одразу додаємо розрахунок базового прогнозу (Сайт)
+            df['Forecast_MW'] = df['Rad'] * 11.4 * 0.001
+            return df
+    except Exception as e:
+        st.error(f"Помилка сервісу погоди: {e}")
+    
+    return pd.DataFrame()
