e345b7c4acdd3f2997172204c2ac160d03b92aac
k_sergej <50540573+SergejKolesnik@users.noreply.github.com>
2026-04-13 11:45:58 +0300

    Update model_engine.py

--- model_engine.py
+++ model_engine.py
@@ -4,30 +4,39 @@ from sklearn.ensemble import RandomForestRegressor
 
 def train_and_get_insights(df_h, df_f):
     # Повний список бажаних факторів
-    features = ['Forecast_MW', 'CloudCover', 'Temp', 'WindSpeed', 'PrecipProb']
+    all_features = ['Forecast_MW', 'CloudCover', 'Temp', 'WindSpeed', 'PrecipProb']
     
-    # Вибираємо тільки ті, що реально є в CSV
-    existing_features = [c for c in features if c in df_h.columns]
+    # 1. Вибираємо тільки ті колонки, які реально існують у вашому CSV
+    existing_features = [c for c in all_features if c in df_h.columns]
     
-    # Якщо бази немає або вона порожня - не ламаємо програму
-    if 'Forecast_MW' not in existing_features or len(df_h.dropna(subset=['Fact_MW'])) < 10:
+    # 2. Фільтруємо дані: беремо рядки, де є і Факт, і хоча б Прогноз сайту
+    # На основі вашої бази (image_ada354.png), у нас вже є 489 рядків
+    df_train = df_h.dropna(subset=['Fact_MW', 'Forecast_MW'])
+    
+    if len(df_train) < 20: # Поріг для старту
         return df_f['Forecast_MW'], 0.0, None, None
 
-    # Навчання на тому, що маємо
-    df_train = df_h.dropna(subset=existing_features + ['Fact_MW'])
-    X = df_train[existing_features]
+    # Навчання на наявних факторах
+    X = df_train[existing_features].fillna(0)
     y = df_train['Fact_MW']
 
     model = RandomForestRegressor(n_estimators=100, random_state=42)
     model.fit(X, y)
 
-    # Прогноз
-    predictions = model.predict(df_f[existing_features])
+    # 3. Прогноз для головного графіка
+    # Використовуємо ті ж колонки, що були в навчанні
+    X_future = df_f[existing_features].fillna(0)
+    predictions = model.predict(X_future)
     accuracy = model.score(X, y) * 100
 
-    # Аналітика
-    importance = pd.DataFrame({'Фактор': existing_features, 'Важливість': model.feature_importances_})
-    error_df = df_train[['Time', 'Fact_MW', 'Forecast_MW']].tail(50).copy()
+    # 4. Аналітика для вкладки Навчання
+    importance = pd.DataFrame({
+        'Фактор': existing_features,
+        'Важливість': model.feature_importances_
+    }).sort_values(by='Важливість', ascending=False)
+
+    # Дельта для візуалізації
+    error_df = df_train[['Time', 'Fact_MW', 'Forecast_MW']].tail(100).copy()
     error_df['Error'] = error_df['Fact_MW'] - error_df['Forecast_MW']
     
     return predictions, accuracy, importance, error_df
